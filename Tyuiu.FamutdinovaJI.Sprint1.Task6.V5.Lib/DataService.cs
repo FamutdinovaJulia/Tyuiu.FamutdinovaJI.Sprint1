ab0624d808937fd0c6ea1185ec055c078ad53148
FamutdinovaJulia <i212195.73@gmail.com>
2024-12-27 19:55:58 +0500

    task6

--- Tyuiu.FamutdinovaJI.Sprint1.Task6.V5.Lib/DataService.cs
+++ Tyuiu.FamutdinovaJI.Sprint1.Task6.V5.Lib/DataService.cs
@@ -10,8 +10,9 @@ namespace Tyuiu.FamutdinovaJI.Sprint1.Task6.V5.Lib
 
         public static string toArray(string srr)
         {
-            char[] norm = srr.ToCharArray();
-            return new string (norm);   
+            string[] words = phrase.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
         }
         public static string Reverse(string s)
         {
