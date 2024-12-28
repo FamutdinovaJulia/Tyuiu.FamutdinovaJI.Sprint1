9999c7de148e1a0599e1b3345b293bddd715c96d
FamutdinovaJulia <i212195.73@gmail.com>
2024-12-28 11:33:48 +0500

    0

--- Tyuiu.FamutdinovaJI.Sprint1.Task5.V3.Lib/DataService.cs
+++ Tyuiu.FamutdinovaJI.Sprint1.Task5.V3.Lib/DataService.cs
@@ -7,9 +7,9 @@ namespace Tyuiu.FamutdinovaJI.Sprint1.Task5.V3.Lib
     {
         public int Calculate(int k)
         {
-            double res0 = (k % 1000) / 100;
-            double res = Math.Floor(res0);
-            return (int)res;
+            int res0 = (k % 1000) / 100;
+            //int res = Math.Floor(res0);
+            return res0;
         }
     }
 }
--- Tyuiu.FamutdinovaJI.Sprint1.Task6.V5.Lib/DataService.cs
+++ Tyuiu.FamutdinovaJI.Sprint1.Task6.V5.Lib/DataService.cs
@@ -5,15 +5,10 @@ namespace Tyuiu.FamutdinovaJI.Sprint1.Task6.V5.Lib
 {
     public class DataService : ISprint1Task6V5
     {
-        public static string toArray(string srr)
-        {
-            string[] words = srr.Split(' ');
-
-            for (int i = 0; i < words.Length; i++) ;
         }
-        public static string Reverse(string s)
+        public string CheckSymmetricalWords(string value)
         {
-            char[] str = s.ToCharArray();
+            char[] str = value.ToCharArray();
             Array.Reverse(str);
             return new string(str);
         }
